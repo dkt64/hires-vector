fb59e5542b41650914364a44b332ac7dd1aa0cb5
DKT <b.apanasewicz@gmail.com>
2020-03-24 22:26:49 +0100

    poprawa skrypu do kopiowania zdalnego

---
+++ copy-main-files-to-server-from-outside.bat
@@ -0,0 +1,2 @@
+c:\tools\pscp -i %USERPROFILE%\.ssh\pi.ppk -r -v dist pi@sidcloud.net:/home/pi/hires-vector
+REM c:\tools\pscp -i %USERPROFILE%\.ssh\pi.ppk hires-vector.go pi@sidcloud.net:/home/pi/hires-vector
--- copy-main-files-to-server.bat
+++ copy-main-files-to-server.bat
@@ -1,2 +1,4 @@
 c:\tools\pscp -i %USERPROFILE%\.ssh\pi.ppk -r -v dist pi@192.168.1.158:/home/pi/hires-vector
 c:\tools\pscp -i %USERPROFILE%\.ssh\pi.ppk hires-vector.go pi@192.168.1.158:/home/pi/hires-vector
+rem list files:
+rem c:\tools\pscp -i %USERPROFILE%\.ssh\pi.ppk -ls pi@sidcloud.net:/home/pi
