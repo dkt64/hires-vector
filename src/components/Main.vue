9a458c0701595d1a060b067a1e6a8886a0670354
DKT <b.apanasewicz@gmail.com>
2020-03-28 22:49:41 +0100

    cleanup

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,66 +1,67 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col>
-        <v-list-item-title class="headline">Input window</v-list-item-title>
-        <div v-on:click="clickOnContainer" id="canvasGL" style="width: 320px; height: 200px"></div>
-        <v-chip
-          :text-color="backgroundColorText"
-          :color="backgroundColorBack"
-          class="mt-1"
-        >Background color</v-chip>
-      </v-col>
-      <v-col>
-        <v-list-item-title class="headline">Hires window</v-list-item-title>
+    <v-row class="d-flex justify-space-around">
+      <v-card tile outlined class="mx-auto pa-2">
+        <v-toolbar color="indigo" class="headline" dark>Three.js</v-toolbar>
+        <div v-on:click="clickOnGL" id="canvasGL" style="width: 320px; height: 200px"></div>
+      </v-card>
+      <v-card tile outlined class="mx-auto pa-2">
+        <v-toolbar color="indigo" class="headline" dark>Hires</v-toolbar>
         <canvas id="canvasHires" width="320" height="200"></canvas>
-      </v-col>
-      <v-col>
-        <v-row>
-          <v-switch v-model="switch_controls" label="Animate" class="headline"></v-switch>
-        </v-row>
-        <v-row>
-          <v-slider
-            class="headline mr-5 mb-5"
-            :thumb-size="24"
-            thumb-label="always"
-            v-model="slider_x"
-            step="1"
-            min="0"
-            max="360"
-            label="Angle X"
-          ></v-slider>
-        </v-row>
-        <v-row>
-          <v-slider
-            class="headline mr-5 mb-5"
-            :thumb-size="24"
-            thumb-label="always"
-            v-model="slider_y"
-            step="1"
-            min="0"
-            max="360"
-            label="Angle Y"
-          ></v-slider>
-        </v-row>
-        <v-row>
-          <v-slider
-            class="headline mr-5"
-            :thumb-size="24"
-            thumb-label="always"
-            v-model="slider_z"
-            step="1"
-            min="0"
-            max="360"
-            label="Angle Z"
-          ></v-slider>
-        </v-row>
-      </v-col>
+      </v-card>
+      <v-card tile outlined class="mx-auto pa-2">
+        <v-toolbar color="indigo" class="headline" dark>Diff</v-toolbar>
+        <canvas id="canvasDiff" width="320" height="200"></canvas>
+      </v-card>
+    </v-row>
+    <v-divider class="mt-5"></v-divider>
+    <v-row class="mt-8 d-flex justify-space-around">
+      <v-slider
+        class="ml-5"
+        :thumb-size="24"
+        thumb-label="always"
+        v-model="slider_x"
+        step="1"
+        min="0"
+        max="360"
+        label="Angle X"
+      ></v-slider>
+      <v-slider
+        class="ml-5"
+        :thumb-size="24"
+        thumb-label="always"
+        v-model="slider_y"
+        step="1"
+        min="0"
+        max="360"
+        label="Angle Y"
+      ></v-slider>
+      <v-slider
+        class="ml-5 mr-2"
+        :thumb-size="24"
+        thumb-label="always"
+        v-model="slider_z"
+        step="1"
+        min="0"
+        max="360"
+        label="Angle Z"
+      ></v-slider>
+    </v-row>
+    <v-row class="d-flex justify-start">
+      <v-switch v-model="switch_controls" label="Animate"></v-switch>
+    </v-row>
+    <v-row>
+      <v-chip :text-color="backgroundColorText" :color="backgroundColorBack">Background color [mouse click]</v-chip>
     </v-row>
   </v-container>
 </template>
 
 <script>
 import * as THREE from "three";
+
+var pixelsGL = new Uint8Array(320 * 200 * 4);
+var changeBackColor = false;
+var backgroundColor = [0, 0, 0, 1];
 
 export default {
   name: "Main",
@@ -74,7 +75,6 @@ export default {
     slider_y: 0,
     slider_z: 0,
     switch_controls: false,
-    backgroundColor: [0, 0, 0, 1],
     backgroundColorBack: "#000000ff",
     backgroundColorText: "#ffffffff"
   }),
@@ -92,23 +92,8 @@ export default {
         ")"
       );
     },
-    clickOnContainer: function(event) {
-
+    clickOnGL: function(event) {
       var canvasGL = document.getElementById("canvasGL");
-
-      var contextGL = this.renderer.getContext();
-
-      var pixelsGL = new Uint8Array(320 * 200 * 4);
-
-      contextGL.readPixels(
-        0,
-        0,
-        320,
-        200,
-        contextGL.RGBA,
-        contextGL.UNSIGNED_BYTE,
-        pixelsGL
-      );
 
       console.log("mouse clicked ", event);
       console.log("this.canvasGL ", canvasGL);
@@ -117,35 +102,24 @@ export default {
       console.log("mouse clicked at ", x, " ", y);
 
       var offset = x * 4 + y * 320 * 4;
+      console.log("offset: " + offset);
 
-      this.backgroundColor[0] = pixelsGL[offset + 0];
-      this.backgroundColor[1] = pixelsGL[offset + 1];
-      this.backgroundColor[2] = pixelsGL[offset + 2];
-      this.backgroundColor[3] = pixelsGL[offset + 3];
-
-      this.backgroundColorBack = this.colorString(
-        this.backgroundColor[0],
-        this.backgroundColor[1],
-        this.backgroundColor[2],
-        this.backgroundColor[3]
-      );
-
-      this.backgroundColorText = this.colorString(
-        this.backgroundColor[0] ^ 0x80,
-        this.backgroundColor[1] ^ 0x80,
-        this.backgroundColor[2] ^ 0x80,
-        this.backgroundColor[3]
-      );
+      backgroundColor[0] = pixelsGL[offset + 0];
+      backgroundColor[1] = pixelsGL[offset + 1];
+      backgroundColor[2] = pixelsGL[offset + 2];
+      backgroundColor[3] = pixelsGL[offset + 3];
 
       console.log(
         "New background color: " +
           this.colorString(
-            this.backgroundColor[0],
-            this.backgroundColor[1],
-            this.backgroundColor[2],
-            this.backgroundColor[3]
+            backgroundColor[0],
+            backgroundColor[1],
+            backgroundColor[2],
+            backgroundColor[3]
           )
       );
+
+      changeBackColor = true;
     },
     init: function() {
       var canvasGL = document.getElementById("canvasGL");
@@ -181,10 +155,7 @@ export default {
       this.scene.add(this.izo);
 
       this.renderer = new THREE.WebGLRenderer({ antialias: true });
-      this.renderer.setSize(
-        canvasGL.clientWidth,
-        canvasGL.clientHeight
-      );
+      this.renderer.setSize(canvasGL.clientWidth, canvasGL.clientHeight);
       this.renderer.setScissor(0, 0, 320, 200);
       this.renderer.setScissorTest(true);
       canvasGL.appendChild(this.renderer.domElement);
@@ -211,8 +182,6 @@ export default {
 
       var contextGL = this.renderer.getContext();
 
-      var pixelsGL = new Uint8Array(320 * 200 * 4);
-
       contextGL.readPixels(
         0,
         0,
@@ -226,6 +195,9 @@ export default {
       var canvasHires = document.getElementById("canvasHires");
       var contextHires = canvasHires.getContext("2d");
 
+      var canvasDiff = document.getElementById("canvasDiff");
+      var contextDiff = canvasDiff.getContext("2d");
+
       var pixelsHires = contextHires.getImageData(0, 0, 320, 200);
       var k = 0;
       for (var j = 200; j > 0; --j) {
@@ -234,7 +206,30 @@ export default {
         }
       }
 
+      // Hires
+      //
+
+      // Zapis do output
       contextHires.putImageData(pixelsHires, 0, 0);
+      contextDiff.putImageData(pixelsHires, 0, 0);
+
+      // Zmiana background color na podstawie kliknięcia myszką
+      if (changeBackColor) {
+        this.backgroundColorBack = this.colorString(
+          backgroundColor[0],
+          backgroundColor[1],
+          backgroundColor[2],
+          backgroundColor[3]
+        );
+        if (
+          (backgroundColor[0] + backgroundColor[0] + backgroundColor[0]) / 3 <
+          0x80
+        ) {
+          this.backgroundColorText = this.colorString(0xff, 0xff, 0xff, 0xff);
+        } else {
+          this.backgroundColorText = this.colorString(0, 0, 0, 0xff);
+        }
+      }
     }
   },
   mounted() {
